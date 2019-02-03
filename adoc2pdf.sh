2ba08a2107b72014e72e9e8f99486c818ea3ce05
Dr. Gernot Starke <gs@gernotstarke.de>
2019-02-03 09:26:15 +0100

    complicated the script to allow some customization

--- adoc2pdf.sh
+++ adoc2pdf.sh
@@ -2,16 +2,52 @@
 
 FILE_PATH=${1:-example.txt}
 FILE=$(basename "$FILE_PATH")
+ADOC_DIR=`dirname "$FILE_PATH"`
 FILE_NAME="${FILE%.*}"
 
-echo "Transforming $FILE_PATH to PDF"
+
+BUILD_PATH="${PWD}/build/"
+XML_FILE=$FILE_NAME.xml
+ODT_FILE=$FILE_NAME.odt
+
+echo "Transforming $FILE to PDF"
+echo "file basename      : $FILE"
+echo "asciidoc input dir : $ADOC_DIR"
+echo "internal build path: $BUILD_PATH"
+echo "docbook xml file   : $XML_FILE"
+echo "odt file           : $ODT_FILE"
+echo "*******************************************************\n\n"
+
+mkdir $BUILD_PATH
 
 # adoc2docbook
-docker run --rm -v ${PWD}:/documents/ asciidoctor/docker-asciidoctor asciidoctor -b docbook5 $FILE_PATH
+# docker -v: mapping directories from host to container
+# asciidoctor -D: output directory
+printf "Step 1: converting adoc -> docbook xml..."
+docker run --rm \
+   -v $BUILD_PATH:/build   \
+   -v $ADOC_DIR:/documents:ro   \
+   asciidoctor/docker-asciidoctor asciidoctor \
+   --verbose \
+   -b docbook5           \
+   -D /build             \
+   /documents/$FILE_NAME.adoc || { echo "Asciidoc conversion failed"; exit 1; }
+printf "...done\n\n"
 
 # docbook2odt
-docker run --rm -v ${PWD}:/source/ jagregory/pandoc -f docbook -t odt --reference-odt=/source/docbook2odt/template.odt $FILE_NAME.xml -o $FILE_NAME.odt
+printf "Step 2: converting docbook -> odt"
+docker run --rm -v ${PWD}:/template/:ro \
+   -v $BUILD_PATH:/build \
+   jagregory/pandoc -f docbook -t odt --reference-odt=/template/docbook2odt/template.odt \
+   /build/$XML_FILE \
+   -o /build/$ODT_FILE || { echo "odt generation failed"; exit 1; }
+printf "...done\n\n"
 
 # odt2pdf
+echo "Step 3: converting odt to pdf"
 cd odt2pdf && docker build -t libreoffice . && cd ..
-docker run --rm -v ${PWD}:/documents libreoffice $FILE_NAME.odt
+docker run --rm \
+   -v $BUILD_PATH:/documents \
+   libreoffice $ODT_FILE
+
+echo "...done"
